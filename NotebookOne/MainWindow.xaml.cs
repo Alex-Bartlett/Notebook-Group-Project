90c0926d6a77fa55d114ea08cd8f7aa055aa00ab
Alex <78022094+Alex-Bartlett@users.noreply.github.com>
2021-11-04 17:15:22 +0000

    Area to view all notes

--- NotebookOne/MainWindow.xaml.cs
+++ NotebookOne/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace NotebookOne
 		public MainWindow()
 		{
 			InitializeComponent();
-			GetSaveFolder();
+			LoadFilesIntoGrid(FilesGrid);
 		}
 
 		private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
@@ -37,7 +37,7 @@ namespace NotebookOne
 				FileStream fileStream = new FileStream(saveFileDialog.FileName, FileMode.Create);
 				TextRange textRange = new TextRange(rtbTextEditor.Document.ContentStart, rtbTextEditor.Document.ContentEnd);
 				textRange.Save(fileStream, DataFormats.Rtf);
-			}
+			}			
 		}
 
 		private string GetSaveFolder()
@@ -50,6 +50,73 @@ namespace NotebookOne
 				Directory.CreateDirectory(subFolderPath);
 			}
 			return subFolderPath;
+		}
+
+		/// <summary>
+		/// Creates a grid row in the given grid
+		/// </summary>
+		/// <param name="grid">The grid to use</param>
+		/// <param name="name">The name of the row</param>
+		private void CreateGridRow(Grid grid, string name)
+		{
+			RowDefinition row = new RowDefinition();
+			row.Height = new GridLength(20, GridUnitType.Auto);
+			row.Name = name;
+			grid.RowDefinitions.Add(row);
+		}
+
+		private void CreateFileBtn(Grid grid, int gridRow, int gridColumn, string text)
+		{
+			Button btn = new Button();
+			btn.Width = 100;
+			btn.Height = 20;
+			//Set grid positions
+			Grid.SetColumn(btn, gridColumn);
+			Grid.SetRow(btn, gridRow);
+			btn.Name = text;
+			btn.Content = text;
+			btn.Margin = new Thickness(5, 5, 5, 0);
+			//Add button
+			grid.Children.Add(btn);
+		}
+
+		/// <summary>
+		/// Gets all .rtf files in saving directory and returns them as a list
+		/// </summary>
+		/// <returns>A list of all .rtf files in directory</returns>
+		private List<string> GetFilesInDirectory()
+		{
+			//Get the path of the save folder, and generate if it doesn't exist.
+			string folderPath = GetSaveFolder();
+			//Retrieve all files from folder
+			string[] f = Directory.GetFiles(folderPath);
+			List<string> files = f.ToList();
+			//Remove non-rtf files
+			foreach(string filePath in files)
+			{
+				if (!filePath.EndsWith(".rtf"))
+				{
+					files.Remove(filePath);
+				}
+			}
+			return files;
+		}
+
+		/// <summary>
+		/// Takes all files in the saving directory and loads them into a grid.
+		/// </summary>
+		/// <param name="grid">The grid to load files into</param>
+		private void LoadFilesIntoGrid(Grid grid)
+		{
+			List<string> files = GetFilesInDirectory();
+			for(int index = 0; index < files.Count; index++)
+			{
+				string filePath = files[index];
+				string fileName = System.IO.Path.GetFileNameWithoutExtension(filePath);
+				CreateGridRow(grid, fileName);
+				CreateFileBtn(grid, index, 1, fileName);
+				Console.WriteLine("Added button for " + fileName + ". I put it in row " + index);
+			}
 		}
 	}
 }
